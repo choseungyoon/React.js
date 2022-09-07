436312c2bb0bda02afcdd6d02809607806c2ced6
choseungyoon <cho31250@gmail.com>
2022-09-07 18:58:07 +0900

    Done chapter6

---
+++ MOMENTUM/chapter6/js/background.js
@@ -0,0 +1,18 @@
+const images = [
+    "0.jpeg",
+    "1.jpeg",
+    "2.jpeg"
+]
+
+function randomNumber(){
+    return Math.floor(Math.random() * images.length);
+}
+
+const chosenImage = images[randomNumber()];
+
+const bgImage = document.createElement("img");
+bgImage.src = `img/${chosenImage}`
+
+document.body.append(bgImage);
+
+console.log(bgImage);
