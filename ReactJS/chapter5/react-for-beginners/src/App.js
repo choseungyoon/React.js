98dd84229269a0669809021711807e7c6714669c
choseungyoon <cho31250@gmail.com>
2022-09-15 14:18:26 +0900

    Done react chapter 7

--- ReactJS/chapter5/react-for-beginners/src/App.js
+++ ReactJS/chapter5/react-for-beginners/src/App.js
@@ -1,57 +1,25 @@
-import Button from "./Button"
-import styles from "./App.module.css"
-import { useEffect, useState } from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 
-function Hello() {
-  function HiFunc(){
-    console.log("hello");
-    return ByFunc;
-  }
 
-  function ByFunc() {
-    console.log("by");
-  }
-
-  useEffect(HiFunc,[]);
-}
-
-function App() {
-
-  const [counter , setValue] = useState(0);
-  const [keyword , setKeyword] = useState("");
-  const [showing , setShowing] = useState(true );
-
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-  const onShow = () => setShowing((prev) => !prev);
-
-  useEffect(() => {
-    console.log("RUN ONLY ONCE");
-  }, []);
-
-  useEffect(() => {
-    console.log("RUN WHEN KEYWORD CHANGED");
-  }, [keyword]);
-
-  useEffect(() => {
-    console.log("RUN WHEN COUNTER CHANGED");
-  }, [counter]);
-
+function App() {  
   return (
-    <div>
-      {showing ? <Hello/> : null}
-      <hr></hr>
-      <input
-      value={keyword}
-      onChange={onChange} 
-      type="text" 
-      placeholder="Search here...">
-      </input>
-      <h1>{counter}</h1>
-      <button onClick={onClick}>Click me</button>
-      <hr></hr>
-      <button onClick={onShow}>{showing ? "show" : "hide"}</button>
-    </div>
+  <Router>
+    <Switch>
+      <Route path="/movie/:id">
+        <Detail></Detail>
+      </Route>
+      <Route path="/">
+        <Home></Home>
+      </Route>
+    </Switch>
+
+  </Router>
   );
 }
 
